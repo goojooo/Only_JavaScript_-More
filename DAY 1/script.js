f23b6dac767ca2b53225ff4707423818fb26be0b
Nayan.Dahare <nayandahare02@gmail.com>
2025-12-18 20:56:05 +0530

    DAY 1 commit

---
+++ DAY 1/script.js
@@ -0,0 +1,81 @@
+// document.getElementById("myH1").textContent = "Hello this is heading...";
+// document.getElementById("myP").textContent = "Hello this is paragraph...";
+// --------------------------------------------------
+
+// Variable is container that stores a values
+// 1) declaration let x;
+// 2) assignment x = 10;
+
+// let x = 10 ; // declaration + initialization/assignment
+// console.log(x)
+
+// let age = 25;
+
+// String is series of charecters
+// ----------------------------------------------------
+
+// let fullName = "HaHa";
+// let age = 69;
+// let isStudent = false;
+
+// document.getElementById("p1").textContent = `My name is ${fullName}`;
+// document.getElementById("p2").textContent = `My age is ${age}`;
+// document.getElementById("p3").textContent = `Enrolled : ${isStudent}`;
+// ------------------------------------------------------
+
+// Arthmatic operations                                           
+// let student = 30;
+
+// student = student - 1;
+// console.log(student);
+// student = student + 1;
+// console.log(student);
+// student = student * 2;
+// console.log(student);
+// student = student / 2;
+// console.log(student);
+
+// operator precedence
+// 1. paranthesis()
+// 2. exponents
+// 3. multiplication division & modulus
+// 4.addition & substraction
+
+// let result =  1 + 2 * 3 + 4 ** 2;
+// // 4**2 = 16
+// // 2*3 = 6
+// // 1+2 = 3
+// // 16+6+3
+// console.log(result)
+
+// let result2 = 12 % 5 + 8 / 2;
+// // 12%5 = 2
+// // 8/2 = 4
+// // 2 + 4 
+// console.log(result2);
+
+// let result3 = 6 / 2 **(2+5);
+// // 2+5=7
+// // 2**7=128
+// // 6/128=0.4...
+// console.log(result3)
+
+// -------------------------------------------------------
+// HOW 2 ACCEPT INPUT FROM USER
+// 1. window prompt
+// 2.Profession way == html textbox
+
+// let userNmae = prompt("What's upp ?");
+// console.log(userNmae);
+
+// let userName;
+// document.getElementById("sub").onclick = function(){
+// userName = document.getElementById("myText").value;
+// // console.log(userName);
+// document.getElementById("h1").textContent = `Hello ${userName}`
+// }
+
+// -------------------------------------------------------
+// TYPE CONVERSION
+// chnage the datatypes of a values to another (strings , numbers, boolean)
+
