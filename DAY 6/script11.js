7d50a9956e49acfb1f70a567027deff3cc443e3a
Nayan.Dahare <nayandahare02@gmail.com>
2025-12-31 14:19:10 +0530

    Number gussing game completed commit

--- DAY 6/script11.js
+++ DAY 6/script11.js
@@ -6,30 +6,27 @@ const ans = Math.floor(Math.random() * (maxNum - minNum + 1))+minNum ;
 
 // console.log(ans);
 
-let attemps = 0;
+let attempts = 0;
 let guess ;
 let running = true;
 
 while(running){
 
     guess = window.prompt(`Guess a number between ${minNum} - ${maxNum} `)
-    gues = Number(guess);
-    if(isNaN(guess)){
-        window.alert("Please enter valid number...")
-    }
-    else if(guess < minNum || guess > maxNum){
+    guess = Number(guess);
+    if(isNaN(guess) || guess < minNum || guess > maxNum){
         window.alert("Please enter valid number...")
     } else{
-        attemps++;
+        attempts++;
         if(guess < ans){
             window.alert("TOO LOW - Try again...")
         } else if(guess > ans) {
             window.alert("TOO HIGH - Try again...")
 
         } else{
-            window.alert(`CORRECT! answer is ${ans}, It took you ${attemps} attemps...`)
+            window.alert(`CORRECT! answer is ${ans}, It took you ${attempts} attempts...`)
 
-            running == false
+            running = false
         }
     }
 }
